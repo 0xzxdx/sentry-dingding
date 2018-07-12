e5db25f322a14e8d4fae301d59e52b3037575407
anshengme <ianshengme@gmail.com>
2018-07-12 10:04:46 +0800

    修改打包信息

--- setup.py
+++ setup.py
@@ -1,38 +1,37 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-from sentry_dingtalk import VERSION
+from sentry_dingtalk import VERSION, NAME
 
-install_requires = [
-    'sentry>=9.0.0',
-    'requests',
-]
-
-f = open('README.rst')
-readme = f.read()
-f.close()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name='sentry-dingtalk',
+    name=NAME,
     version=VERSION,
     author='ansheng',
     author_email='ianshengme@gmail.com',
     url='https://github.com/anshengme/sentry-dingtalk',
     description='A Sentry extension which send errors stats to DingTalk',
-    long_description=readme,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='MIT',
+    keywords='sentry dingtalk',
+    include_package_data=True,
+    zip_safe=False,
     package_dir={'': 'src'},
     packages=find_packages('src'),
-    install_requires=install_requires,
+    install_requires=[
+        'sentry>=9.0.0',
+        'requests',
+    ],
     entry_points={
         'sentry.plugins': [
             'sentry_dingtalk = sentry_dingtalk.plugin:DingTalkPlugin'
         ]
     },
-    include_package_data=True,
-    zip_safe=False,
     classifiers=[
-        'Programming Language :: Python :: 2.7'
-    ],
-    keywords='sentry dingtalk',
+        'Programming Language :: Python :: 2.7',
+        "License :: OSI Approved :: MIT License",
+    ]
 )
--- src/sentry_dingtalk/__init__.py
+++ src/sentry_dingtalk/__init__.py
@@ -1,3 +1,4 @@
 # coding: utf-8
 
 VERSION = '0.0.1'
+NAME = "sentry_dingtalk"
